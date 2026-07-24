d5d851a9c1b37d5a030e12960feab39d29d3a5d8
MurodjonYakubov <yakubovmurodjon85@gmail.com>
2026-07-24 18:58:24 +0500

    Men hozir JS bo'limini o'rganyapman

--- Js-darslar/yangi/lessons/2-bo'lim/2-dars/index.js
+++ Js-darslar/yangi/lessons/2-bo'lim/2-dars/index.js
@@ -12,9 +12,9 @@ let yil = prompt("Tug'ilgan yilingizni kiriting!")
 let yosh = 2026-yil
 
 if (yosh>0) {
-    console.log("Siz dunyoga kelgansiz.", "Siz", yosh , "yoshdasiz")    
+    console.log(`Siz ${yil}-yili dunyoga kelgansiz.`, "Siz", yosh , "yoshdasiz")    
 }else{
-    console.log("Siz hali tug'ilmagansiz");
+    console.log("Siz hali tug'ilmagansiz, bu yil hali kelmagan");
 }
 
 // Sharpness V
@@ -23,3 +23,4 @@ if (yosh>0) {
 // Mending I
 // Fire Aspect II
 // Sweeping Edge III
+// Knockback II
